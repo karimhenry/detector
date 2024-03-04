2eca60d5d76111357fef6fb44ec88b125d17d8c0
karimhenry <kimohenry94@gmail.com>
2024-03-04 19:53:45 +0200

    app_v1

--- request.py
+++ request.py
@@ -1,26 +1,25 @@
 import requests
 import os
 from flask import Flask, make_response, render_template,send_file
-import datetime
-
 
 app = Flask(__name__,template_folder='.')
-app.config['UPLOAD_FOLDER'] = 'static'
 
 @app.route('/', methods=["GET"] )
 def infer():
-    container_url = "http://localhost:8000"  # Replace with actual alias and port
+    container_url = "http://localhost:8000"  
     try:
         response = requests.get(container_url + "/loc")
         print("Request successful:", response.text)
         output_path = response.text   
         output_path = output_path.split("yolo_detector/")[-1]
+        print(output_path)
         return send_file(output_path)
     except:
         return make_response("<h1>URL Not Found</h1>", 404)
 
 if __name__=="__main__":
     app.run(debug=True)
+
 # # Replace with the container's alias in the network
 # container_url = "http://localhost:8000"  # Replace with actual alias and port
 
